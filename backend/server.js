6c172400c70ee7404b0d70a4a51f115f28e21be9
Vaishnavipol <vaishnavipol2709@gmail.com>
2026-04-21 16:35:54 +0530

    deploye in process

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 connectDB();
@@ -11,5 +12,14 @@ app.use(express.json());
 
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/tasks", require("./routes/taskRoutes"));
+
+
+// static frontend serve
+app.use(express.static(path.join(__dirname, "dist")));
+
+// React routing handle
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
  app.listen(PORT, ()=>console.log(`Server is listening port ${PORT}`));
