fd3b0473dcd7016c061724ad30a2c018a27fab20
Vaishnavipol <vaishnavipol2709@gmail.com>
2026-04-21 17:34:12 +0530

    successfully deployed

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,25 +2,6 @@ const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-// exports.register = async (req, res) => {
-//   const { name, email, password, role } = req.body;
-
-//   try {
-//     const hashedPassword = await bcrypt.hash(password, 10);
-
-//     const user = await User.create({
-//       name,
-//       email,
-//       password: hashedPassword,
-//       role
-//     });
-
-//     res.json(user);
-//   } catch (err) {
-//     res.status(500).json({ message: "Error registering user" });
-//   }
-// };
-
 
 exports.register = async (req, res) => {
   try {
@@ -28,18 +9,18 @@ exports.register = async (req, res) => {
 
     const { name, email, password, role } = req.body;
 
-    // ✅ validation
+    // validation
     if (!name || !email || !password || !role) {
       return res.status(400).json({ message: "All fields are required" });
     }
 
-    // ✅ duplicate check
+    // duplicate check
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res.status(400).json({ message: "User already exists" });
     }
 
-    // ✅ hash password
+    // hash password
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = await User.create({
